485cc86f2ce4443f808d66d7dd71aca13cdc5618
Cristian Sanso <cristian.sanso@mercadolibre.com>
2018-12-10 14:20:10 -0300

    creado componente navigation y todoCrudForm. Creada funcionalidad remove.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/Navigation';
+import TodoCrudForm from './components/TodoCrudForm';
+
+import {todos} from './todos.json';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+  this.handleAddTodo = this.handleAddTodo.bind(this);
+  this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  handleAddTodo(todo) {
+    this.setState ({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index) {
+    this.setState ({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    })
+  }
+
   render() {
+  const todos = this.state.todos.map((todo, i)=>{
+    return(
+      <div className="col-md-4">
+        <div className="card mt-4">
+          <div className="card-header">
+            <h3> {todo.title} </h3>
+            <span className="badge badge-pill badge-danger ml-2">
+              {todo.priority}
+            </span>
+          </div>
+          <div className="card-body">
+            <p> {todo.description} </p>
+            <p> {todo.responsible} </p>
+          </div>
+          <div className="card-footer">
+            <button className="btn btn-danger" onClick={this.removeTodo.bind(this, i)}>
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <Navigation title="Task" numberTask={todos}/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-3">
+
+              <TodoCrudForm onAddTodo={this.handleAddTodo}/>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
